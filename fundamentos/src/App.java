2898ca51ea3c112d62a86c606fdd62ab69c229ce
Samira Pessoal <sixoobecan@gmail.com>
2022-11-08 17:24:39 -0300

    estudo de tipos primitivos

--- fundamentos/src/App.java
+++ fundamentos/src/App.java
@@ -3,9 +3,16 @@ import java.lang;
 
 public class App {
     public static void main(String[] args){
-        Scanner user = new Scanner(System.in);
-        String nome = user.nextLine();
-        float sal = user.nextFloat();
-        System.out.format("o salario de %s é %f", nome, sal);
+        try (Scanner user = new Scanner(System.in)) {
+            String nome = user.nextLine();
+            float sal = user.nextFloat();
+            System.out.format("o salario de %s é %f", nome, sal);
+        }
     }
+}
+public class Soma{
+    int n1 = Integer.parseInt(txtN1.getText());
+    int n2 = Integer.parseInt(txtN2.getText());
+    int soma = n1 + n2;
+    Soma.setText(Integer.toString(soma));
 }
